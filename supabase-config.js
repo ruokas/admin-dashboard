dbc2062f6a3297503172de149d8a9a46ef0e5607
Rokas M <63348397+ruokas@users.noreply.github.com>
2025-11-19 14:33:00 +0200

    Create supabase-config.js from example

--- supabase-config.js
+++ supabase-config.js
@@ -8,5 +8,5 @@
  * Tikrasis `supabase-config.js` failas neturėtų būti versionuojamas –
  * taip išvengsite raktų nutekėjimo.
  */
-export const SUPABASE_URL = '';
-export const SUPABASE_ANON_KEY = '';
+export const SUPABASE_URL = 'https://affzgzmcxkvcsidqgikn.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmZnpnem1jeGt2Y3NpZHFnaWtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1Mzg0ODgsImV4cCI6MjA3OTExNDQ4OH0.sYNGGK-JwxtOJr5--eNi43PitWKtrnD7yyDP2cEFGd8';
